5df097d6999446497c70290eceb820222845a80a
Suveett <75737576+Suveett@users.noreply.github.com>
2021-03-02 22:13:09 +0530

    Update 2_Deploy_coinflip.js

--- 2_Deploy_coinflip.js
+++ 2_Deploy_coinflip.js
@@ -2,8 +2,8 @@ const Coinflip = artifacts.require("Coinflip");
 
 module.exports = function(deployer,network,accounts) {
   deployer.deploy(Coinflip).then(function(instance){
-    instance.fundContract({value : web3.utils.toWei("1", "ether"), from : accounts[0]}).then(function(){
-      console.log("The Contract successfully got funded with 1 ether from Owner address: " + accounts[0]);
+    instance.fundContract({value : web3.utils.toWei("0.01", "ether"), from : accounts[0]}).then(function(){
+      console.log("The Contract successfully got funded with 0.01 ether from Owner address: " + accounts[0]);
       console.log("The Contract address is :" + Coinflip.address);
     }).catch(function(err){
       console.log("The contract couldn't get funded " + err);
