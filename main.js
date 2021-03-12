c0006498df3a9bab9016ffaa762ba84e14ac5a53
Suveett <75737576+Suveett@users.noreply.github.com>
2021-03-12 20:18:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,52 +1,63 @@
 var web3 = new Web3(Web3.givenProvider);
 var contractInstance;
-const contractAddress = "0xeA0114706ba05D9dd49DE48b88b969c4A51713c7";
+const contractAddress = "0x1183647d5616A44db74CcAF35433E23367Fbe271";
+
 
 $(document).ready(async function(){
-  contractInstance = new web3.eth.Contract(abi,contractAddress);
-  await connectMetamask();
 
+  window.ethereum.enable().then(function(accounts){
+    contractInstance = new web3.eth.Contract(abi,contractAddress, {from : accounts[0]});
     console.log(contractInstance);
-    console.log(`Use Contract address: ${contractInstance._address}`)
+    console.log(`Use Contract address: ${contractInstance._address}`);
 
-    $("#flip_button").click(async function(){
-      await flipCoin()
-    });
-    $("#get_balance").click(async function() {
-      await fetchAndDisplay();
-    });
-    $("#fund_contract_button").click(async function(){
-      await fundContract();
-    });
-    $("#withdraw_funds").click(async function(){
-      await withdrawFunds();
-    });
-    $("#withdraw_all_funds").click(async function(){
-      await withdrawAll();
-    });
-    $("#get_balance").click(async function(){
-      await fetchAndDisplay();
-    });
+});
 
-    //EVENT LISTENERS
 
-    contractInstance.once('LogNewProvableQuery',
-    {
-      filter: { player: await getPlayerAddress() },
-      fromBlock: 'latest'
-    }, (error, event) => {
-      if(error) throw("Error fetching events");
-      jQuery("#events").text(`User ${event.returnValues.player} is waiting for the flip result`);
-    });
+        $("#flip_button").click(async function(){
+          await connectMetamask();
+          await flipCoin();
 
-    contractInstance.once('FlipResult',
-    {
-      filter: { player : await getPlayerAddress() },
-      fromBlock: 'latest'
-    }, (error, event) => {
-      if(error) throw("Error fetching events");
-      jQuery("#events").text(`User ${event.returnValues.player} won: ${event.returnValues.amountWon}`);
-    });
+          //EVENT LISTENERS
+          contractInstance.once('LogNewProvableQuery',
+          {
+            fromBlock: 'latest'
+          }, (error, event) => {
+            if(error) throw("Error fetching events");
+            jQuery("#events").text(`User ${event.returnValues.player} is waiting for the flip result`);
+          });
+
+
+
+          contractInstance.once('FlipResult',
+          {
+            fromBlock: 'latest'
+          }, (error, event) => {
+            if(error) throw("Error fetching events");
+            jQuery("#events").text(`User ${event.returnValues.player} won: ${event.returnValues.amountWon}`);
+          });
+        });
+
+
+        $("#get_balance").click(async function() {
+          await fetchAndDisplay();
+        });
+        $("#fund_contract_button").click(async function(){
+          await connectMetamask();
+          await fundContract();
+        });
+        $("#withdraw_funds").click(async function(){
+          await connectMetamask();
+          await withdrawFunds();
+        });
+        $("#withdraw_all_funds").click(async function(){
+          await connectMetamask();
+          await withdrawAll();
+        });
+
+
+
+
+
 
 });
 
@@ -54,24 +65,15 @@ $(document).ready(async function(){
 
 
 
-async function connectMetamask() {
-  if (typeof window.ethereum !== undefined) {
-    const accounts = await web3.eth.requestAccounts();
-    let p = await getPlayerAddress();
-    jQuery("#playerAddress").text(p);
+  async function connectMetamask() {
+    if (typeof window.ethereum !== undefined) {
+      const accounts = await web3.eth.getAccounts();
+      if(accounts[0] !== undefined) {
+        return web3.utils.toChecksumAddress(accounts[0]);
+      }
+      jQuery("#playerAddress").text(accounts[0]);
+    }
   }
-}
-
-async function getPlayerAddress() {
-  const playerAddr = await web3.eth.getAccounts();
-  if(playerAddr[0] !== undefined) {
-    return web3.utils.toChecksumAddress(playerAddr[0]);
-  }
-}
-
-
-
-
 
 
   async function flipCoin(){
@@ -79,7 +81,7 @@ async function getPlayerAddress() {
         var config = {
             value: web3.utils.toWei(bet,"ether")
         }
-        await contractInstance.methods.flipCoin().send(config, {from : await getPlayerAddress()})
+        await contractInstance.methods.flipCoin().send(config)
         .on("transactionHash", function(hash){
             console.log(hash);
         })
@@ -100,7 +102,7 @@ async function getPlayerAddress() {
 
 
       async function fetchAndDisplay(){
-          await contractInstance.methods.getBalance().call({from : await getPlayerAddress()}).then(function(res){
+          await contractInstance.methods.getBalance().call().then(function(res){
             $("#jackpot_output").text("The Contract has : " + web3.utils.fromWei(res[1], "ether") + "Ether");
 
           })
@@ -112,7 +114,7 @@ async function getPlayerAddress() {
         var config = {
           value : web3.utils.toWei(fund, "ether")
         }
-        await contractInstance.methods.fundContract().send(config, {from : await getPlayerAddress()})
+        await contractInstance.methods.fundContract().send(config)
         .on("transactionHash", function(hash){
           console.log(hash);
         })
@@ -128,9 +130,10 @@ async function getPlayerAddress() {
 
 
       async function withdrawFunds(){
-        await contractInstance.methods.withdrawFunds().send({from : await getPlayerAddress()});
+        await contractInstance.methods.withdrawFunds().send();
       };
 
       async function withdrawAll(){
-        await contractInstance.methods.withdrawAll().send({from : await getPlayerAddress()});
-      }
+        await contractInstance.methods.withdrawAll().send();
+      };
+
